e955203a70c5b42638bc746e00c716b7829a1e05
nduka ojeh <nojeh@baneollc.com>
2024-07-10 18:34:39 -0400

    run files

---
+++ run.sh
@@ -0,0 +1,3 @@
+git pull
+#ansible-playbook -i frontend.dev.banecio-devops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=frontend main.yml
+ansible-playbook -i $1.dev.banecio-devops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
